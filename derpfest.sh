57d7988f9b2acd5a6db3155c68f0c0f735463f3c
nothing-sus-2 <nagarjun7400@gmail.com>
2024-12-08 21:59:34 +0530

    Update derpfest.sh

--- derpfest.sh
+++ derpfest.sh
@@ -6,7 +6,7 @@ repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 13
 /opt/crave/resync.sh
 # cloning DT
 # device tree
-git clone https://github.com/Night-Raids-Reborn/device_xiaomi_citrus.git --depth 1 -b thirteen device/xiaomi/citrus
+git clone https://github.com/nothing-sus-2/device_xiaomi_citrus.git --depth 1 -b thirteen device/xiaomi/citrus
 git clone https://github.com/nothing-sus-2/device_xiaomi_sm6115-common.git  --depth 1 -b thirteen device/xiaomi/sm6115-common
 # kernel tree
 git clone https://github.com/greenforce-project/kernel_xiaomi_citrus_sm6115.git --depth 1 -b 13.0.0-perf kernel/xiaomi/sm6115
@@ -15,6 +15,9 @@ git clone https://github.com/Night-Raids-Reborn/vendor_xiaomi_citrus.git --depth
 git clone https://github.com/Night-Raids-Reborn/vendor_xiaomi_sm6115-common.git --depth 1 -b thirteen vendor/xiaomi/sm6115-common
 # hardware tree
 git clone https://github.com/asterixiverz/hardware_xiaomi.git --depth 1 -b thirteen-aosp hardware/xiaomi
+
+#signing keys
+git clone https://github.com/shravansayz/private_keys.git -b rise vendor/lineage-priv
 # set build environment
 . build/envsetup.sh
 #Preparing device
