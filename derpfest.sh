66ef04b94866133ea9f01e85220bd7791da39a32
nothing-sus-2 <nagarjun7400@gmail.com>
2024-12-09 20:47:57 +0530

    Update derpfest.sh

--- derpfest.sh
+++ derpfest.sh
@@ -16,6 +16,9 @@ git clone https://github.com/Night-Raids-Reborn/vendor_xiaomi_sm6115-common.git 
 # hardware tree
 git clone https://github.com/asterixiverz/hardware_xiaomi.git --depth 1 -b thirteen-aosp hardware/xiaomi
 
+#miui camera
+git clone https://gitlab.com/ThankYouMario/proprietary_vendor_xiaomi_camera.git --depth 1 -b uvite-sm6225 vendor/xiaomi/camera
+
 #signing
 git clone https://github.com/Rv-Trees/vendor_lineage-priv_keys.git -b orion vendor/lineage-priv/keys
 
