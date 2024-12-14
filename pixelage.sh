cd880743673d13279e39779cd65e640d32fdbbe7
nothing-sus-2 <nagarjun7400@gmail.com>
2024-12-14 19:55:39 +0530

    Update pixelage.sh

--- pixelage.sh
+++ pixelage.sh
@@ -3,7 +3,6 @@
 rm -rf device/xiaomi/chime
 rm -rf kernel/xiaomi/chime
 rm -rf vendor/xiaomi/chime
-rm -rf hardware/xiaomi
 rm -rf vendor/lineage-priv/keys
 
 # Initialize ROM manifest
@@ -17,11 +16,10 @@ git clone https://github.com/nothing-sus-2/device_xiaomi_chime.git --depth 1 -b 
 git clone https://github.com/Joe7500/kernel_xiaomi_chime.git --depth 1 -b ksu kernel/xiaomi/chime
 # vendor tree
 git clone https://github.com/Joe7500/vendor_xiaomi_chime.git --depth 1 -b lineage-22 vendor/xiaomi/chime
-# hardware tree
-git clone https://github.com/asterixiverz/hardware_xiaomi.git --depth 1 -b thirteen-aosp hardware/xiaomi
 
 #signing
 git clone https://github.com/Rv-Trees/vendor_lineage-priv_keys.git -b orion vendor/lineage-priv/keys
+
 export PIXELAGE_BUILD="chime"
 
 #lunch
