bbe7ac8e64b6c7387a444cd0a558de07e5427e73
nothing-sus-2 <nagarjun7400@gmail.com>
2024-12-16 18:08:20 +0530

    Update ethereal.sh

--- ethereal.sh
+++ ethereal.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
 
-rm -rf device/xiaomi/citrus
-rm -rf device/xiaomi/sm6115-common
-rm -rf kernel/xiaomi/electro
-rm -rf vendor/xiaomi/citrus
-rm -rf vendor/xiaomi/sm6115-common
-rm -rf hardware/xiaomi
-rm -rf vendor/lineage-priv/keys
+rm -rf device/xiaomi/chime
+rm -rf kernel/xiaomi/chime
+rm -rf vendor/xiaomi/chime
+
 
 # Initialize ROM manifest
-  repo init -u https://github.com/Ethereal-OS/Manifest -b A13 --git-lfs
+repo init -u https://github.com/VoltageOS/manifest.git -b 15 --git-lfs
 # repo sync
 /opt/crave/resync.sh
 # cloning DT
 # device tree
-git clone https://github.com/nothing-sus-2/device_xiaomi_citrus.git --depth 1 -b 13 device/xiaomi/citrus
-git clone https://github.com/nothing-sus-2/device_xiaomi_sm6115-common.git --depth -b thirteen device/xiaomi/sm6115-common
+git clone https://github.com/nothing-sus-2/device_xiaomi_chime2.git --depth 1 device/xiaomi/chime
 # kernel tree
-git clone https://github.com/hac4us06/kernel-xiaomi-electro.git --depth 1 -b 14 kernel/xiaomi/electro
+git clone https://github.com/nnippon99/kernel_xiaomi_chime.git --depth 1 -b viola-ksu kernel/xiaomi/chime
 # vendor tree
-git clone https://github.com/Night-Raids-Reborn/vendor_xiaomi_citrus.git --depth 1 -b thirteen vendor/xiaomi/citrus
-git clone https://github.com/Night-Raids-Reborn/vendor_xiaomi_sm6115-common.git --depth 1 -b thirteen vendor/xiaomi/sm6115-common
-# hardware tree
-git clone https://github.com/asterixiverz/hardware_xiaomi.git --depth 1 -b thirteen-aosp hardware/xiaomi
+git clone https://github.com/hac4us06/vendor_xiaomi_chime.git --depth 1 -b 14 vendor/xiaomi/chime
 
+#extras
+rm -rf hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git --depth 1 -b lineage-22.0 hardware/xiaomi
+cd vendor/voltage/config
+wget -O device_framework_matrix.xml https://raw.githubusercontent.com/LineageOS/android_vendor_lineage/refs/heads/lineage-21.0/config/device_framework_matrix.xml
+cd -
 #signing
-git clone https://github.com/Rv-Trees/vendor_lineage-priv_keys.git -b orion vendor/lineage-priv/keys
+git clone https://github.com/VoltageOS/vendor_voltage-priv_keys vendor/voltage-priv/keys
+cd vendor/voltage-priv/keys
+./keys.sh
+cd -
 
-# set build environment
+#lunch
 . build/envsetup.sh
-#Preparing device
-lunch ethreal_citrus-user
-# lunch
-make ethereal
+brunch citrus
