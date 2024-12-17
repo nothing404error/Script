d422adc04dc0b45ed50e344dd44482d121e9a05e
nothing-sus-2 <nagarjun7400@gmail.com>
2024-12-17 20:32:39 +0530

    Update and rename voltage.sh to infinity.sh

--- infinity.sh
+++ infinity.sh
@@ -6,12 +6,12 @@ rm -rf vendor/xiaomi/chime
 
 
 # Initialize ROM manifest
-repo init -u https://github.com/VoltageOS/manifest.git -b 15 --git-lfs
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault
 # repo sync
 /opt/crave/resync.sh
 # cloning DT
 # device tree
-git clone https://github.com/nothing-sus-2/device_xiaomi_chime2.git --depth 1 device/xiaomi/chime
+git clone https://github.com/nothing-sus-2/device_xiaomi_chime2.git --depth 1 -b infinity15 device/xiaomi/chime
 # kernel tree
 git clone https://github.com/nnippon99/kernel_xiaomi_chime.git --depth 1 -b viola-ksu kernel/xiaomi/chime
 # vendor tree
@@ -20,12 +20,9 @@ git clone https://github.com/hac4us06/vendor_xiaomi_chime.git --depth 1 -b 14 ve
 #extras
 rm -rf hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git --depth 1 -b lineage-22.0 hardware/xiaomi
-cd vendor/voltage/config
-wget -O device_framework_matrix.xml https://raw.githubusercontent.com/LineageOS/android_vendor_lineage/refs/heads/lineage-21.0/config/device_framework_matrix.xml
-cd -
 #signing
-git clone https://github.com/VoltageOS/vendor_voltage-priv_keys vendor/voltage-priv/keys
-cd vendor/voltage-priv/keys
+git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys
+cd vendor/infinity-priv/keys
 ./keys.sh
 cd -
 
