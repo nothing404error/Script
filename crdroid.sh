14bde718a0ebf15c7977946655ebbe5cdac4683f
nothing-sus-2 <nagarjun7400@gmail.com>
2025-01-05 17:46:36 +0530

    Update crdroid.sh

--- crdroid.sh
+++ crdroid.sh
@@ -3,7 +3,6 @@
 rm -rf device/xiaomi/chime
 rm -rf kernel/xiaomi/chime
 rm -rf vendor/xiaomi/chime
-rm -rf hardware/xiaomi
 rm -rf vendor/lineage-priv/keys
 
 # Initialize ROM manifest
@@ -17,8 +16,6 @@ git clone https://github.com/nothing-sus-2/android_device_xiaomi_chime.git --dep
 git clone https://github.com/Joe7500/kernel_xiaomi_chime.git --depth 1 -b ksu kernel/xiaomi/chime
 # vendor tree
 git clone https://github.com/Joe7500/android_vendor_xiaomi_chime.git --depth 1 -b 13.0 vendor/xiaomi/chime
-# hardware tree
-git clone https://github.com/asterixiverz/hardware_xiaomi.git --depth 1 -b thirteen-aosp hardware/xiaomi
 
 #signing
 git clone https://github.com/Rv-Trees/vendor_lineage-priv_keys.git -b orion vendor/lineage-priv/keys
@@ -26,9 +23,7 @@ git clone https://github.com/Rv-Trees/vendor_lineage-priv_keys.git -b orion vend
 #misc
 rm -rf vendor/crdroid
 git clone https://github.com/nothing-sus-2/android_vendor_crdroid.git -b 13.0 vendor/crdroid
-git clone https://github.com/nothing-sus-2/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
 
-git clone https://gitlab.com/ThankYouMario/proprietary_vendor_xiaomi_camera.git -b uvite-sm6225 vendor/xiaomi/camera
 
 # set build environment
 . build/envsetup.sh
