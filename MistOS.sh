eb7a1c6ec933ce6a47c52c773e7524a32e25074c
nothing-sus-2 <nagarjun7400@gmail.com>
2025-01-14 13:15:44 +0530

    Update and rename crdroid.sh to MistOS.sh

---
+++ MistOS.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+rm -rf device/xiaomi/chime
+rm -rf kernel/xiaomi/chime
+rm -rf vendor/xiaomi/chime
+rm -rf hardware/xiaomi
+
+# Initialize ROM manifest
+repo init -u https://github.com/Project-Mist-OS/manifest -b 15 --git-lfs
+# repo sync
+/opt/crave/resync.sh
+# cloning DT
+# device tree
+git clone https://github.com/nothing-sus-2/device_xiaomi_chime.git --depth 1 -b lineage-22.1 device/xiaomi/chime
+# kernel tree
+git clone https://github.com/Joe7500/kernel_xiaomi_chime.git --depth 1 -b ksu kernel/xiaomi/chime
+# vendor tree
+git clone https://github.com/Joe7500/vendor_xiaomi_chime.git --depth 1 -b lineage-22 vendor/xiaomi/chime
+# hardware tree
+git clone https://github.com/asterixiverz/hardware_xiaomi.git --depth 1 -b thirteen-aosp hardware/xiaomi
+
+
+# set build environment
+. build/envsetup.sh
+#Preparing device
+mistify chime user
+# lunch
+mist sb
