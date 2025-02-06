3005102adee9d3205108ef97e8f7ba8ced82da2f
nothing-sus-2 <nagarjun7400@gmail.com>
2025-02-06 18:07:20 +0530

    Update MistOS.sh

--- MistOS.sh
+++ MistOS.sh
@@ -6,21 +6,23 @@ repo init -u https://github.com/Project-Mist-OS/manifest -b 15 --git-lfs
 /opt/crave/resync.sh
 # cloning DT
 # device tree
-git clone https://github.com/nothing-sus-2/device_motorola_fogos.git --depth 1 -b 15 device/motorola/fogos
-git clone https://github.com/OptoHub/device_motorola_sm6375-common.git --depth 1 -b 15 device/motorola/sm6375-common
+git clone https://github.com/nothing-sus-2/android_device_motorola_fogos.git --depth 1 -b fifteen device/motorola/fogos
+git clone https://github.com/Motorola-SM6375-Devs/android_device_motorola_sm6375-common.git --depth 1 -b fifteen device/motorola/sm6375-common
 # kernel tree
-git clone https://github.com/OptoHub/kernel_motorola_sm6375.git --depth 1 -b 15 kernel/motorola/sm6375
+git clone https://github.com/Motorola-SM6375-Devs/android_kernel_motorola_sm6375.git --depth 1 -b fifteen kernel/motorola/sm6375
 # vendor tree
-git clone https://github.com/OptoHub/vendor_motorola_fogos.git --depth 1 -b 14 vendor/motorola/fogos
-git clone https://github.com/OptoHub/vendor_motorola_sm6375-common.git --depth 1 -b 15 vendor/motorola/sm6375-common
+git clone https://gitlab.com/Motorola-SM6375-Devs/proprietary_vendor_motorola_fogos.git --depth 1 -b fifteen vendor/motorola/fogos
+git clone https://gitlab.com/Motorola-SM6375-Devs/proprietary_vendor_motorola_sm6375-common.git --depth 1 -b fifteen vendor/motorola/sm6375-common
 # hardware tree
 rm -rf hardware/motorola
-git clone https://github.com/vullk4n/hardware_motorola.git --depth 1 hardware/motorola
+rm -rf system/qcom
+git clone https://github.com/LineageOS/android_hardware_motorola.git hardware/motorola
+git clone https://github.com/LineageOS/android_system_qcom.git system/qcom
 
 
 # set build environment
 . build/envsetup.sh
 #Preparing device
-mistify fogos userdebug
+mistify fogos user
 # lunch
 mist b
