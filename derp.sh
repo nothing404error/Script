2d61a4b9c2fa1a033ae3409102035583f1a82c44
nothing-sus-2 <nagarjun7400@gmail.com>
2025-03-09 22:53:42 +0530

    Update and rename sup.sh to derp.sh

---
+++ derp.sh
@@ -0,0 +1,6 @@
+# set build environment
+. build/envsetup.sh
+#Preparing device
+lunch derp_citrus-userdebug
+# lunch
+mka derp
--- sup.sh
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/hac4us06/hardware_xiaomi hardware/xiaomi
-
-# set build environment
-. build/envsetup.sh
-#Preparing device
-lunch superior_chime-userdebug
-# lunch
-m bacon
