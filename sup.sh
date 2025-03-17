a922f2246e3bb4069db41887db581f85f99e4490
nothing-sus-2 <nagarjun7400@gmail.com>
2025-03-17 23:39:12 +0530

    Update sup.sh

--- sup.sh
+++ sup.sh
@@ -1,8 +1,6 @@
-rm -rf device/xiaomi/chime
-git clone https://github.com/nothing-sus-2/device_xiaomi_chime.git -b 13.0 device/xiaomi/chime
 # set build environment
 . build/envsetup.sh
 #Preparing device
-lunch ethereal_chime-userdebug
+lunch derp_chime-userdebug
 # lunch
-make ethereal
+mka derp
