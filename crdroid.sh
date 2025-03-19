66c39964dbf1f70633a417b29cf0fea3bdb4f280
nothing-sus-2 <nagarjun7400@gmail.com>
2025-03-19 21:09:14 +0530

    Update crdroid.sh

--- crdroid.sh
+++ crdroid.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Initialize ROM manifest
-repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
+repo init -u https://github.com/crDroid11/android.git -b 11.0 --git-lfs
 # repo sync
 /opt/crave/resync.sh
 # cloning DT
 # device tree
-git clone https://github.com/nothing-sus-2/device_xiaomi_chime.git --depth 1 -b lineage-22.1 device/xiaomi/chime
+git clone https://github.com/nothing-sus-2/android_device_xiaomi_chime.git --depth 1 -b 11.0 device/xiaomi/chime
 # kernel tree
 git clone https://github.com/Joe7500/kernel_xiaomi_chime.git --depth 1 -b main kernel/xiaomi/chime
 # vendor tree
-git clone https://github.com/Joe7500/vendor_xiaomi_chime.git --depth 1 -b lineage-22 vendor/xiaomi/chime
+git clone https://github.com/Night-Raids-Reborn/vendor_xiaomi_chime-r.git --depth 1 -b eleven vendor/xiaomi/chime
 
 
 # set build environment
 . build/envsetup.sh
 #Preparing device
-lunch lineage_chime-user
+lunch lineage_chime-userdebug
 # lunch
 brunch chime
