aabd76659ed6fe8bf966c372d17a6752b852f501
nothing-sus-2 <nagarjun7400@gmail.com>
2025-04-07 15:02:20 +0530

    Update crdroid.sh

--- crdroid.sh
+++ crdroid.sh
@@ -1,9 +1,6 @@
-rm -rf device/xiaomi/chime
-git clone https://github.com/nothing-sus-2/device_xiaomi_chime.git -b test device/xiaomi/chime
-
 # set build environment
 . build/envsetup.sh
 #Preparing device
-lunch lineage_chime-user
+lunch infinity_citrus-userdebug
 # lunch
-m bacon
+mka bacon
