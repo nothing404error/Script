cb464f1517c499c5fd8203d078095cbcc4b1ca1c
nothing-sus-2 <nagarjun7400@gmail.com>
2025-04-24 22:49:31 +0530

    Update and rename pe.sh to arrow.sh

---
+++ arrow.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Initialize ROM manifest
+repo init -u https://github.com/Arrow-OS-Extended/android_manifest.git -b arrow-13.1 --git-lfs
+# repo sync
+/opt/crave/resync.sh
+# cloning DT
+# device tree
+git clone https://github.com/nothing-sus-2/device_xiaomi_chime.git --depth 1 -b Test device/xiaomi/chime
+# kernel tree
+git clone https://github.com/nnippon99/kernel_xiaomi_chime.git --depth 1 -b viola kernel/xiaomi/chime
+# vendor tree
+git clone https://github.com/nothing-sus-2/vendor_xiaomi_chime.git --depth 1 vendor/xiaomi/chime
+#misc
+rm -rf vendor/arrow
+git clone https://github.com/nothing-sus-2/android_vendor_arrow.git --depth 1 vendor/arrow
+#hardware tree
+git clone https://github.com/asterixiverz/hardware_xiaomi.git --depth 1 hardware/xiaomi
+
+# set build environment
+. build/envsetup.sh
+#Preparing device
+lunch arrow_chime-userdebug
+# lunch
+m bacon
