ac6699dd294f79b172ae481d4ad166d3e9ca6ab2
nothing-sus-2 <nagarjun7400@gmail.com>
2025-05-02 16:13:46 +0530

    Update crdroid.sh

--- crdroid.sh
+++ crdroid.sh
@@ -1,3 +1,5 @@
+rm -rf device/xiaomi/chime
+git clone https://github.com/nothing-sus-2/device_xiaomi_chime.git -b test device/xiaomi/chime
 rm -rf packages/apps/Updater
 git clone https://github.com/nothing-sus-2/android_packages_apps_Updater.git -b 13.0 packages/apps/Updater
 # set build environment
