77cae1b8c0199804b1d57b38e2f39ab305e27fc7
nothing-sus-2 <nagarjun7400@gmail.com>
2025-05-10 21:44:09 +0530

    Update derp.sh

--- derp.sh
+++ derp.sh
@@ -19,5 +19,7 @@ git clone https://github.com/LineageOS/android_hardware_xiaomi.git --depth 1 -b 
 
 # set build environment
 . build/envsetup.sh
+#Preparing device
+lunch lineage_citrus-userdebug
 # lunch
-derpfest citrus
+mka bacon
