7bd321399a29292ac007dd9c9a758b8b920f26bf
nothing-sus-2 <nagarjun7400@gmail.com>
2025-05-11 14:23:30 +0530

    Update derp.sh

--- derp.sh
+++ derp.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+rm -rf device/xiaomi/chime
+rm -rf vendor/xiaomi/chime
+rm -rf vendor/xiaomi/citrus
+rm -rf vendor/xiaomi/lime
+rm -rf kernel/xiaomi/chime
+
 # Initialize ROM manifest
 repo init -u https://github.com/DerpFest-LOS/android_manifest.git -b 15.2 --git-lfs
 # repo sync
 /opt/crave/resync.sh
 # cloning DT
 # device tree
-git clone https://github.com/nothing-sus-2/device_xiaomi_chime15.git --depth 1 -b derp device/xiaomi/chime
+git clone https://github.com/nothing-sus-2/device_xiaomi_chime.git --depth 1 -b derp device/xiaomi/chime
 # kernel tree
-git clone https://github.com/nnippon99/kernel_xiaomi_chime.git --depth 1 -b viola-ksu kernel/xiaomi/chime
+git clone https://github.com/Joe7500/kernel_xiaomi_chime.git --depth 1 -b ksu kernel/xiaomi/chime
 # vendor tree
-git clone https://gitlab.com/nnippon99/vendor_xiaomi_chime.git --depth 1 -b viola vendor/xiaomi/chime
-git clone https://gitlab.com/nnippon99/vendor_xiaomi_citrus.git --depth 1 -b viola vendor/xiaomi/citrus
-git clone https://gitlab.com/nnippon99/vendor_xiaomi_lime.git --depth 1 -b viola vendor/xiaomi/lime
+git clone https://github.com/Joe7500/vendor_xiaomi_chime.git --depth 1 -b lineage-22.2 vendor/xiaomi/chime
 
 #hardware tree
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git --depth 1 -b lineage-22.2 hardware/xiaomi
@@ -20,6 +24,6 @@ git clone https://github.com/LineageOS/android_hardware_xiaomi.git --depth 1 -b 
 # set build environment
 . build/envsetup.sh
 #Preparing device
-lunch lineage_citrus-bp1a-userdebug
+lunch lineage_chime-bp1a-userdebug
 # lunch
 mka bacon
