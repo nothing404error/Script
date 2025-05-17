f08493405a4222a5484d5f1dae7124c60695f2e0
nothing-sus-2 <nagarjun7400@gmail.com>
2025-05-17 23:08:21 +0530

    Update arrow.sh

--- arrow.sh
+++ arrow.sh
@@ -1,3 +1,5 @@
+rm -rf packages/apps/Updater
+git clone https://github.com/nothing-sus-2/android_packages_apps_Updater.git packages/apps/Updater
 rm -rf kernel/xiaomi/chime
 git clone https://github.com/nothing-sus-2/kernel_xiaomi_chime.git -b viola kernel/xiaomi/chime
 # set build environment
