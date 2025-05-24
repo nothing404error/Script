964862a5fa9ad32509d8918979430092d7900cce
nothing-sus-2 <nagarjun7400@gmail.com>
2025-05-24 12:32:54 +0530

    Update and rename sup.sh to genesis.sh

---
+++ genesis.sh
@@ -0,0 +1,6 @@
+# set build environment
+source build/envsetup.sh
+#Preparing device
+lunch genesis_citrus-bp1a-userdebug
+# lunch
+mka genesis
--- sup.sh
+++
@@ -1,6 +0,0 @@
-# set build environment
-. build/envsetup.sh
-#Preparing device
-lunch lineage_chime-bp1a-userdebug
-# lunch
-mka derp
