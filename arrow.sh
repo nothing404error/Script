8ad69abe999886bbe28072a73a9cbff1e4a3cd09
nothing-sus-2 <nagarjun7400@gmail.com>
2025-05-29 12:05:47 +0530

    Update arrow.sh

--- arrow.sh
+++ arrow.sh
@@ -1,5 +1,5 @@
 rm -rf packages/apps/Launcher3
-git clone https://github.com/nothing-sus-2/android_packages_apps_Launcher3.git packages/apps/Launcher3
+git clone https://github.com/Arrow-OS-Extended/android_packages_apps_Launcher3.git packages/apps/Launcher3
 # set build environment
 . build/envsetup.sh
 #Preparing device
