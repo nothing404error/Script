2556a7be593c2305cb677479bd7c8aa2df582d20
nothing-sus-2 <nagarjun7400@gmail.com>
2025-05-30 20:34:41 +0530

    Update arrow.sh

--- arrow.sh
+++ arrow.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-# Initialize ROM manifest
-repo init -u https://github.com/Ethereal-OS/Manifest -b A13 --git-lfs
-# repo sync
-/opt/crave/resync.sh
-# cloning DT
-# device tree
-git clone https://github.com/nothing-sus-2/device_xiaomi_chime.git --depth 1 -b ethereal device/xiaomi/chime
-
 # set build environment
 . build/envsetup.sh
 #Preparing device
