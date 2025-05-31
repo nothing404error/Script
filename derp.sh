1809ed215260bfe876807bd57d9a1254aa89077f
nothing-sus-2 <nagarjun7400@gmail.com>
2025-05-31 23:39:30 +0530

    Update derp.sh

--- derp.sh
+++ derp.sh
@@ -12,10 +12,12 @@ git clone https://github.com/nothing-sus-2/device_xiaomi_sm6115-common.git devic
 git clone https://github.com/greenforce-project/kernel_xiaomi_citrus_sm6115.git --depth 1 kernel/xiaomi/sm6115
 # vendor tree
 git clone https://github.com/nothing-sus-2/vendor_xiaomi_sm6115-common.git --depth 1 vendor/xiaomi/sm6115-common
-
 git clone https://github.com/Night-Raids-Reborn/vendor_xiaomi_citrus.git vendor/xiaomi/citrus
-#hardware tree
 
+#misc
+git clone https://github.com/PixelExperience/packages_resources_devicesettings.git -b thirteen packages/resources/devicesettings
+#hardware tree
+git clone https://github.com/asterixiverz/hardware_xiaomi.git hardware/xiaomi
 # set build environment
 . build/envsetup.sh
 #Preparing device
