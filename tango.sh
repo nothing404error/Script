4d9cc65721f65eeb980c944d19298f4e1f59327e
nothing-sus-2 <nagarjun7400@gmail.com>
2025-06-01 10:26:35 +0530

    Update tango.sh

--- tango.sh
+++ tango.sh
@@ -1,8 +1,11 @@
 rm -rf packages/apps/DerpLauncher
 git clone https://github.com/nothing-sus-2/packages_apps_DerpLauncher.git -b 13 packages/apps/DerpLauncher
+#misc
+rm -rf packages/resources/devicesettings
+git clone https://github.com/PixelExperience/packages_resources_devicesettings.git -b thirteen packages/resources/devicesettings
 # set build environment
 . build/envsetup.sh
 #Preparing device
-lunch derp_chime-user
+lunch derp_citrus-user
 # lunch
 mka derp
