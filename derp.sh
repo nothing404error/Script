c5c18d61af4872ed5147428b95d55af6387b6ef8
nothing-sus-2 <nagarjun7400@gmail.com>
2025-06-04 17:25:36 +0530

    Update derp.sh

--- derp.sh
+++ derp.sh
@@ -1,26 +1,6 @@
-#!/bin/bash
-
-# Initialize ROM manifest
-repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 13
-# repo sync
-/opt/crave/resync.sh
-# cloning DT
-# device tree
-git clone https://github.com/nothing-sus-2/device_xiaomi_citrus.git --depth 1 device/xiaomi/citrus
-git clone https://github.com/nothing-sus-2/device_xiaomi_sm6115-common.git device/xiaomi/sm6115-common
-# kernel tree
-git clone https://github.com/greenforce-project/kernel_xiaomi_citrus_sm6115.git --depth 1 kernel/xiaomi/sm6115
-# vendor tree
-git clone https://github.com/nothing-sus-2/vendor_xiaomi_sm6115-common.git --depth 1 vendor/xiaomi/sm6115-common
-git clone https://github.com/Night-Raids-Reborn/vendor_xiaomi_citrus.git vendor/xiaomi/citrus
-
-#misc
-git clone https://github.com/PixelExperience/packages_resources_devicesettings.git -b thirteen packages/resources/devicesettings
-#hardware tree
-git clone https://github.com/asterixiverz/hardware_xiaomi.git hardware/xiaomi
 # set build environment
 . build/envsetup.sh
 #Preparing device
-lunch derp_citrus-user
+lunch lineage_chime-bp1a-user
 # lunch
 mka derp
