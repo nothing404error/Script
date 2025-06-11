46ed1066a4c195e29b36261f0069c57516079b78
nothing-sus-2 <nagarjun7400@gmail.com>
2025-06-11 22:06:33 +0530

    Update and rename crdroid.sh to sup.sh

--- crdroid.sh
+++
@@ -1,9 +0,0 @@
-/opt/crave/resync.sh
-rm -rf packages/apps/Updater
-git clone https://github.com/nothing-sus-2/android_packages_apps_Updater.git -b 13.0 packages/apps/Updater
-# set build environment
-. build/envsetup.sh
-#Preparing device
-lunch lineage_chime-user
-# lunch
-m bacon
