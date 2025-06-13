28a41985fbb5d056a2d39fa8a569f54ffb8fa973
nothing-sus-2 <nagarjun7400@gmail.com>
2025-06-13 14:59:40 +0530

    Update sup.sh

--- sup.sh
+++ sup.sh
@@ -1,6 +1,3 @@
-/opt/crave/resync.sh
-rm -rf device/xiaomi/chime
-git clone https://github.com/nothing-sus-2/android_device_xiaomi_chime.git -b sup device/xiaomi/chime
 # set build environment
 . build/envsetup.sh
 #Preparing device
