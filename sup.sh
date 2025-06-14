858daf196798633deb5a6e6959df2cd9a7add340
nothing-sus-2 <nagarjun7400@gmail.com>
2025-06-14 21:27:43 +0530

    Update sup.sh

--- sup.sh
+++ sup.sh
@@ -1,7 +1,8 @@
-#Clone Rom
+# Clone Rom
 repo init --no-repo-verify -u https://github.com/superior-lts/manifest.git -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
-#repo sync
+# repo sync
 /opt/crave/resync.sh
+#clone device trees
 rm -rf device/xiaomi/chime
 git clone https://github.com/nothing-sus-2/android_device_xiaomi_chime.git -b sup device/xiaomi/chime
 # set build environment
