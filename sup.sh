a8b6997a02693ca910a180c3e42db778fc0229ce
nothing-sus-2 <nagarjun7400@gmail.com>
2025-06-15 12:54:14 +0530

    Update sup.sh

--- sup.sh
+++ sup.sh
@@ -1,5 +1,8 @@
 rm -rf kernel/xiaomi/chime
 git clone https://github.com/nnippon99/kernel_xiaomi_chime.git -b viola kernel/xiaomi/chime
+#clone device trees
+rm -rf device/xiaomi/chime
+git clone https://github.com/nothing-sus-2/android_device_xiaomi_chime.git -b sup device/xiaomi/chime
 # set build environment
 . build/envsetup.sh
 #Preparing device
