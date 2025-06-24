95242833d68842f2ee62ffecb04d1010855df780
nothing-sus-2 <anagarjun7400@gmail.com>
2025-06-24 23:55:07 +0530

    Update arrow.sh

--- arrow.sh
+++ arrow.sh
@@ -1,7 +1,7 @@
-rm -rf packages/apps/ViPER4AndroidFX
+
 # set build environment
 . build/envsetup.sh
 #Preparing device
-lunch ethereal_chime-user
+lunch arrow_chime-user
 # lunch
-make ethereal
+mka bacon
