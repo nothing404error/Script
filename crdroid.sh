c313688e78d0398f221674c6b885f205efe33ee3
nothing-sus-2 <anagarjun7400@gmail.com>
2025-06-27 13:18:16 +0530

    Update crdroid.sh

--- crdroid.sh
+++ crdroid.sh
@@ -1,7 +1,27 @@
+#!/bin/bash
+
+rm -rf device/xiaomi/chime
+rm -rf vendor/xiaomi/chime
+rm -rf kernel/xiaomi/chime
+# Rom source repo
+repo init -u https://github.com/crdroid-13-fork/android.git -b 13.0 --git-lfs
+
+# Sync the repositories
 /opt/crave/resync.sh
-# set build environment
+
+# Clone Device trees
+git clone https://github.com/nothing-sus-2/android_device_xiaomi_chime.git -b test --depth 1 device/xiaomi/chime
+git clone https://github.com/nothing-sus-2/vendor_xiaomi_chime.git -b 13.0 --depth 1 vendor/xiaomi/chime
+git clone https://github.com/nnippon99/kernel_xiaomi_chime.git -b viola --depth 1 kernel/xiaomi/chime
+
+#Hardware trees
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+ 
+# Set up build environment
 . build/envsetup.sh
-#Preparing device
+
+# Lunch
 lunch lineage_chime-user
-# lunch
+
+# Build rom
 m bacon
