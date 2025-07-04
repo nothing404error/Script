5a91bd3de85574eabdb3c67ecbe49204806ed238
nothing-sus-2 <anagarjun7400@gmail.com>
2025-07-04 16:01:48 +0530

    Update and rename cr.sh to breeze.sh

---
+++ breeze.sh
@@ -0,0 +1,6 @@
+# Set up build environment
+source build/envsetup.sh
+# Lunch
+breakfast breeze user
+# Build rom
+mka bacon
--- cr.sh
+++
@@ -1,6 +0,0 @@
-# Set up build environment
-. build/envsetup.sh
-# Lunch
-lunch lineage_chime-user
-# Build rom
-m bacon
