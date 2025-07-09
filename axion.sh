ec451da4a72b67eceafe520e7cd90fff09aa2428
nothing-sus-2 <anagarjun7400@gmail.com>
2025-07-09 22:37:07 +0530

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -1,30 +1,7 @@
-#!/bin/bash
-
-rm -rf device/xiaomi/chime
-rm -rf vendor/xiaomi/chime
-rm -rf kernel/xiaomi/chime
-# Rom source repo
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.2 --git-lfs
-
-# Sync the repositories
-/opt/crave/resync.sh
-
-# Clone Device trees
-git clone https://github.com/nothing-sus-2/device_xiaomi_chime.git -b axion --depth 1 device/xiaomi/chime
-git clone https://github.com/Joe7500/vendor_xiaomi_chime.git -b lineage-22.2 --depth 1 vendor/xiaomi/chime
-git clone https://github.com/nnippon99/kernel_xiaomi_chime.git -b viola --depth 1 kernel/xiaomi/chime
-echo 'CONFIG_SCHED_DEBUG=y' >> kernel/xiaomi/chime/arch/arm64/configs/vendor/xiaomi/bengal_defconfig
-
-#Hardware trees
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi
- 
 # Set up build environment
 source build/envsetup.sh
-
 # Lunch
 axion chime user vanilla
-
-
 # Build rom
 ax -b user
 
