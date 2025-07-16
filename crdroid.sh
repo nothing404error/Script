b4d2e6678a7ed7ea50e0f5bb2ddcb2dc6cbd5ad7
nothing404error <anagarjun7400@gmail.com>
2025-07-16 15:15:21 +0530

    Update crdroid.sh

--- crdroid.sh
+++ crdroid.sh
@@ -10,13 +10,21 @@ repo init -u https://github.com/crdroid-13-fork/android.git -b 13.0 --git-lfs
 /opt/crave/resync.sh
 
 # Clone Device trees
-git clone https://github.com/nothing-sus-2/android_device_xiaomi_chime.git -b test --depth 1 device/xiaomi/chime
+git clone https://github.com/ArrowOS-T-Fork/device_xiaomi_chime.git -b test --depth 1 device/xiaomi/chime
 git clone https://github.com/nothing-sus-2/vendor_xiaomi_chime.git -b 13.0 --depth 1 vendor/xiaomi/chime
 git clone https://github.com/nnippon99/kernel_xiaomi_chime.git -b viola --depth 1 kernel/xiaomi/chime
 
 #Hardware trees
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
- 
+
+#misc
+rm -rf packages/apps/crDroidSettings
+git clone https://github.com/nothing404error/android_packages_apps_crDroidSettings.git packages/apps/crDroidSettings
+rm -rf packages/apps/Updater
+git clone https://github.com/nothing404error/android_packages_apps_Updater.git packages/apps/Updater
+rm -rf external/bouncycastle
+git clone https://github.com/ProjectMatrixx/external_bouncycastle.git external/bouncycastle
+
 # Set up build environment
 . build/envsetup.sh
 
