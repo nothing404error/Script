a5b69e0a8333151104b7eb39be85b165912adb35
nothing404error <anagarjun7400@gmail.com>
2025-07-16 23:09:43 +0530

    Update rv.sh

--- rv.sh
+++ rv.sh
@@ -1,5 +1,3 @@
-rm -rf packages/apps/crDroidSettings
-git clone https://github.com/nothing404error/android_packages_apps_crDroidSettings.git packages/apps/crDroidSettings
 # Set up build environment
 . build/envsetup.sh
 # Lunch
