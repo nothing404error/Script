ce9edc4bc1b0f71231700ed808add44081d56a75
nothing404error <anagarjun7400@gmail.com>
2025-08-08 19:48:54 +0530

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+# Rom source repo
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
+# Sync the repositories
+/opt/crave/resync.sh
 # Set up build environment
 source build/envsetup.sh
 # Lunch
