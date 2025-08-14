a50a2d23a73815c1e65ef341f2fc12cc2725cf19
nothing404error <anagarjun7400@gmail.com>
2025-08-15 00:15:10 +0530

    Update luna.sh

--- luna.sh
+++ luna.sh
@@ -1,3 +1,9 @@
+# Rom source repo
+repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs
+
+# Sync the repositories
+/opt/crave/resync.sh
+
 #Hardware trees
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi
 
