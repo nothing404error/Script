1750dbfa36d430d485325a3063b6ac68a82b3f97
nothing404error <anagarjun7400@gmail.com>
2025-08-26 20:33:22 +0530

    Update BaikalOS.sh

--- BaikalOS.sh
+++ BaikalOS.sh
@@ -1,3 +1,5 @@
+rm -rf packages/modules/Bluetooth
+git clone https://github.com/nothing404error/android_packages_modules_Bluetooth.git packages/modules/Bluetooth
 # Set up build environment
 . build/envsetup.sh
 
