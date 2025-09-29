8d1e74f5f339454adf26a51b196818b0d55cba7c
nothing404error <anagarjun7400@gmail.com>
2025-09-29 16:41:38 +0530

    Update axion15.sh

--- axion15.sh
+++ axion15.sh
@@ -1,5 +1,5 @@
-rm -rf packages/apps/Launcher3
-git clone https://github.com/nothing404error/android_packages_apps_Launcher3.git -b lineage-22.2 packages/apps/Trebuchet
+rm -rf packages/apps/DocumentsUI
+git clone https://github.com/nothing404error/android_packages_apps_DocumentsUI.git packages/apps/DocumentsUI
 # Set up build environment
 source build/envsetup.sh
 source build/envsetup.sh
