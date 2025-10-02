63b3b94f273637d3a2602b33088eb8dc7de1ea18
nothing404error <anagarjun7400@gmail.com>
2025-10-02 19:43:03 +0530

    Update matrix15.sh

--- matrix15.sh
+++ matrix15.sh
@@ -1,7 +1,7 @@
 rm -rf packages/apps/DocumentsUI
 git clone https://github.com/nothing404error/android_packages_apps_DocumentsUI.git packages/apps/DocumentsUI
 # Set up build environment
-build/envsetup.sh
+. build/envsetup.sh
 # Lunch
 lunch lineage_citrus-bp1a-user
 mka installclean
